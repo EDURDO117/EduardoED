3f8c4881ff8652ae18585d91ae47bd107e6e42cf
Eduardo <em0512752@gmail.com>
2026-06-22 12:28:22 -0600

    Refactor altas.h for improved structure and clarity

--- altas.h
+++ altas.h
@@ -1,7 +1,14 @@
+#ifndef ALTAS_H
+#define ALTAS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Persona *nuevaPersona();
+struct alumno *nuevoAlumno();
+
 void altas(struct Persona **ptr){
+
     struct Persona *p = NULL;
     struct alumno *a = NULL;
 
@@ -16,6 +23,7 @@ void altas(struct Persona **ptr){
 
     if(a == NULL){
         printf("Error al crear alumno\n");
+        free(p->nombre);
         free(p);
         return;
     }
@@ -25,7 +33,8 @@ void altas(struct Persona **ptr){
 
     if(*ptr == NULL){
         *ptr = p;
-    }else{
+    }
+    else{
         struct Persona *aux = *ptr;
 
         while(aux->ptrSig != NULL){
@@ -35,35 +44,45 @@ void altas(struct Persona **ptr){
         aux->ptrSig = p;
     }
 
-    
-    printf("Registro agregado correctamente.\n");
+    printf("\nRegistro agregado correctamente\n");
 }
+
 struct Persona *nuevaPersona(){
+
     struct Persona *p;
 
     p = (struct Persona *)malloc(sizeof(struct Persona));
 
     if(p != NULL){
+
         p->nombre = (char *)malloc(50*sizeof(char));
+
+        if(p->nombre == NULL){
+            free(p);
+            return NULL;
+        }
 
         printf("Nombre: ");
         scanf(" %[^\n]", p->nombre);
 
         printf("Edad: ");
-        scanf("%d",&p->edad);
+        scanf("%d", &p->edad);
 
-        printf("Genero: ");
-        scanf(" %c",&p->genero);
+        printf("Genero (M/F): ");
+        scanf(" %c", &p->genero);
 
-        printf("Fecha nacimiento: ");
-        scanf("%7s",p->fn);
+        printf("Fecha de nacimiento: ");
+        scanf("%8s", p->fn);
 
+        p->ptrAlum = NULL;
         p->ptrSig = NULL;
     }
 
     return p;
 }
+
 struct alumno *nuevoAlumno(){
+
     struct alumno *a;
 
     a = (struct alumno *)malloc(sizeof(struct alumno));
@@ -71,24 +90,27 @@ struct alumno *nuevoAlumno(){
     if(a != NULL){
 
         printf("Matricula: ");
-        scanf("%7s",a->matricula);
+        scanf("%7s", a->matricula);
 
         printf("Carrera: ");
-        scanf("%4s",a->carrera);
+        scanf("%4s", a->carrera);
 
         printf("Semestre: ");
-        scanf("%d",&a->semestre);
+        scanf("%d", &a->semestre);
 
         printf("Correo: ");
-        scanf("%22s",a->correo);
+        scanf("%22s", a->correo);
 
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                printf("Calif[%d][%d]: ",i+1,j+1);
-                scanf("%f",&a->calif[i][j]);
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+
+                printf("Calificacion [%d][%d]: ", i+1, j+1);
+                scanf("%f", &a->calif[i][j]);
             }
         }
     }
 
     return a;
 }
+
+#endif
